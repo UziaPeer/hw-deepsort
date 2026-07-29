78ea91066ef1649fe4b92d9b516134022ca9ebe0
Uzia Peer <uzia.Peer@msmail.ariel.ac.il>
2026-07-29 16:41:12 +0300

    Refactor deep_sorted function for cleaner syntax

--- deepsort.py
+++ deepsort.py
@@ -1,29 +1,17 @@
 def deep_sorted(x:any)->str:
- def key(v):
-        if isinstance(v, dict):
-            return (3, [(key(k), key(v[k])) for k in sorted(v, key=key)])
-        if isinstance(v, (list, tuple)):
-            return (2, [key(i) for i in v])
-        if isinstance(v, set):
-            return (1, sorted(key(i) for i in v))
-        return (0, v)
-
     if isinstance(x, dict):
-        return "{" + ", ".join(f"{deep_sorted(k)}: {deep_sorted(x[k])}" for k in sorted(x, key=key)) + "}"
+        return "{" + ", ".join(deep_sorted(k) + ": " + deep_sorted(x[k]) for k in sorted(x)) + "}"
     if isinstance(x, list):
-        return "[" + ", ".join(deep_sorted(i) for i in sorted(x, key=key)) + "]"
+        return "[" + ", ".join(deep_sorted(i) for i in sorted(x)) + "]"
     if isinstance(x, tuple):
-        items = [deep_sorted(i) for i in sorted(x, key=key)]
-        return "(" + ", ".join(items) + ("," if len(items) == 1 else "") + ")"
+        return "(" + ", ".join(deep_sorted(i) for i in sorted(x)) + ")"
     if isinstance(x, set):
-        if not x:
-            return "set()"
-        return "{" + ", ".join(deep_sorted(i) for i in sorted(x, key=key)) + "}"
-    return repr(x)
+        return "{" + ", ".join(deep_sorted(i) for i in sorted(x)) + "}"
+    return str(x)
 
 
 if __name__ == '__main__':
     # x=eval(input())
     # print(deep_sorted(x))
     import doctest
-    print (doctest.testmod())
+    print(doctest.testmod())
